d42c9c3c1211017de45cdf9dd87b432c3122a97e
JHOAN PATRICIO SASNALEMA DE LA CRUZ <jhoan.sasnalema@epn.edu.ec>
2025-11-03 18:35:54 -0500

    update 2,1

--- src/Main.java
+++ src/Main.java
@@ -15,55 +15,55 @@ public class Main {
         System.out.print("Ingrese el número de términos a generar: ");
         int n = sc.nextInt();
 
-        System.out.println("\n=== SERIE 1 ===");
+        System.out.println("\n=== SERIE NUMERICA 1 ===");
         S1Numerica serie1 = new S1Numerica();
         serie1.mostrarSeries(n);
 
 
-        System.out.println("\n=== SERIE 2 ===");
+        System.out.println("\n=== SERIE NUMERICA 2 ===");
         S2Numerica serie2 = new S2Numerica();
         serie2.mostrarSeries(n);
 
 
-        System.out.println("\n=== SERIE 3 ===");
+        System.out.println("\n=== SERIE NUMERICA 3 ===");
         S3Numerica serie3 = new S3Numerica();
         serie3.mostrarSeries(n);
 
 
-        System.out.println( "\n=== SERIE 4 ===");
+        System.out.println( "\n=== SERIE NUMERICA 4 ===");
         S4Numerica serie4 = new S4Numerica();
         serie4.mostrarSeries(n);
 
-        System.out.println("\n=== SERIE 5 ==="   );
+        System.out.println("\n=== SERIE NUMERICA 5 ==="   );
         S5Numerica serie5 = new S5Numerica();
         serie5.mostrarSeries(n);
 
 
 
-        System.out.println("\n=== SERIE 6 ===");
+        System.out.println("\n=== SERIE NUMERICA 6 ===");
         S6Numerica serie6 = new S6Numerica();
         serie6.mostrarSeries(n);
 
 
-        System.out.println("\n=== SERIE 7 ===");
+        System.out.println("\n=== SERIE NUMERICA 7 ===");
         S7Numerica serie7 = new S7Numerica();
         serie7.mostrarSeries(n);
 
-        System.out.println("\n=== SERIE 8 ===");
+        System.out.println("\n=== SERIE NUMERICA 8 ===");
         S8Numerica serie8 = new S8Numerica();
         serie8.mostrarSeries(n);
 
-        System.out.println("\n=== SERIE 9 ===");
+        System.out.println("\n=== SERIE NUMERICA 9 ===");
         S9Numerica serie9 = new S9Numerica();
         serie9.mostrarSeries(n);
 
 
-        System.out.println("\n=== SERIE 10 ===");
+        System.out.println("\n=== SERIE NUMERICA 10 ===");
         S10Numerica serie10 = new S10Numerica();
         serie10.mostrarSeries(n);
 
 
-        System.out.println("\n=== SERIE 12 ===");
+        System.out.println("\n=== SERIE NUMERICA 12 ===");
         S12Numerica serie12 = new S12Numerica();
         serie12.mostrarSeries(n);
 
@@ -107,7 +107,7 @@ public class Main {
         System.out.println("\n=== SERIE DE CARACTERES 9 ===");
         S9Caracteres serieS9 = new S9Caracteres();
         serieS9.mostrarSeries(n);
-        
+
 
 
         sc.close();
